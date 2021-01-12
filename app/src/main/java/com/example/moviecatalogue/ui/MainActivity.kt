a297abc30645457c99da195e2aebc93e10d14970
Jova <jovaandrea8721@gmail.com>
2021-01-12 17:16:31 +0700

    remove unused resources

--- app/src/main/java/com/example/moviecatalogue/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/ui/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.moviecatalogue.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviecatalogue.R
 import com.example.moviecatalogue.databinding.ActivityMainBinding
@@ -15,18 +17,42 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_movie, R.id.navigation_tv, R.id.navigation_search
-        ).build()
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, binding.navigationLayout)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (intArrayOf(R.id.navigation_favorite_movie, R.id.navigation_favorite_tv).contains(
+                    destination.id
+                )
+            ) {
+                bottomNavigation.visibility = View.GONE
+            } else {
+                bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+
+        bottomNavigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 }
